9ce0ae9e5ed138c4947e7b6a52c9355d2d4436d9
Vikrant-K-Thakur <vikrantkthakur123@gmail.com>
2024-10-12 16:41:48 +0530

    Update Even_Odd.py

--- Even_Odd.py
+++ Even_Odd.py
@@ -1,8 +1,16 @@
-print("Its a program to check even or odd")
+i = 1
+while i!=0:
+    print("Its a program to check even or odd")
 
-num = int(input("Enter any number: "))
+    num = input("Enter any number: ")
+    if num.isdigit() or (num[0] == '-' and num[1:].isdigit()):
+    
+        if(int(num)%2==0):
+            print("Its even number")
+        elif(int(num)%2!=0):
+                print("Its odd number")
 
-if(num%2==0):
-    print("Its even number")
-else:
-    print("Its odd number")
+        i = int(input("You have to check another number?\nwrite \"1\" for \"Yes\" or \"0\" for \"No\": "))
+    else:
+        print("Invalid input! please write valid number")
+print("Thank for using the program")
